c323a02e7d7afdc5d2a57203e7c8311f4cafef7e
WannJJ <76657794+WannJJ@users.noreply.github.com>
2026-06-11 16:46:12 +0200

    feat: prevent default context menu while clicking sidebar

--- frontend/src/components/layout/Sidebar/SideBar.jsx
+++ frontend/src/components/layout/Sidebar/SideBar.jsx
@@ -1,7 +1,21 @@
 import { useUIStore } from "@/stores/useUIStore";
+import { useEffect } from "react";
 import styles from "./SideBar.module.css";
 export default function SideBar({ children }) {
   const { sidebarOpen, setSidebarOpen } = useUIStore();
+
+  useEffect(() => {
+    const handler = (e) => {
+      // Chỉ prevent nếu click vào vùng của sidebar
+      //if (e.target.closest(".sidebar")) {
+      if (e.target.closest(`.${styles.sidebar}`)) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("contextmenu", handler);
+    return () => document.removeEventListener("contextmenu", handler);
+  }, []);
+
   return (
     <>
       {/* Overlay để đóng sidebar khi bấm ra ngoài */}
