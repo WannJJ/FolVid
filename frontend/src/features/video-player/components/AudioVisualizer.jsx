b1959930b3ff8571e8cc632c8a5d868b7750d5e0
WannJJ <76657794+WannJJ@users.noreply.github.com>
2026-06-13 18:40:54 +0200

    fix(AudioVisualizer): fix text animation when playing non-video file

--- frontend/src/features/video-player/components/AudioVisualizer.jsx
+++ frontend/src/features/video-player/components/AudioVisualizer.jsx
@@ -1,13 +1,19 @@
 import { useVideoStore } from "@/stores/useVideoStore";
+import { usePlayer } from "../contexts/PlayerContext";
 import styles from "./AudioVisualizer.module.css";
 
 export function AudioVisualizer() {
+  const { isPlaying } = usePlayer();
   const { currentVideo } = useVideoStore();
   return (
     <div className={styles.audioVisualizer}>
       <div className={styles.marqueeTrack}>
         {/* Hiển thị đơn giản cho file mp3 */}
-        <span className={styles.marqueeText}>🎵 {currentVideo.filename}</span>
+        <span
+          className={`${styles.marqueeText} ${isPlaying ? styles.active : ""}`}
+        >
+          🎵 {currentVideo.filename}
+        </span>
       </div>
     </div>
   );
